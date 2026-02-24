12147d90ebfb98d6f9e172c36365ecac6b762747
cielivree <cielivree93@gmail.com>
2026-02-24 20:12:08 +0200

    Binary search

---
+++ binary-search.js
@@ -0,0 +1,23 @@
+// for already sorted arrays only!
+
+const arr = [1, 4, 68, 129, 1020, 1400, 2640, 9280]
+
+function binarySearch(arr, target) {
+    let left = 0
+    let right = arr.length - 1
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2)
+        const guess = arr[mid]
+
+        if (guess === target) {
+            return mid    // element found
+        } else if (guess < target) {
+            left = mid + 1     // target's in the bigger side of an array
+        } else {
+            right = mid - 1      // target's in the smaller side of an array
+        }
+    }
+
+    return undefined
+}
